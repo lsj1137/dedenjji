5e526570b8a699b2d0c65d7b930995b4cee5492c
LimSeJun <lsj1137@naver.com>
2025-05-10 23:29:59 +0900

    Feat: 제비뽑기 기능 완료

---
+++ src/app/draw/draw-result/draw-list.tsx
@@ -0,0 +1,118 @@
+'use client';
+import Card from '@/components/Card';
+import { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRefresh } from '@fortawesome/free-solid-svg-icons';
+import Button from '@/components/Button';
+
+type DrawListProps = {
+  total: number;
+  win: number;
+};
+
+type DrawItem = {
+  x: number;
+  y: number;
+  color: string;
+  num: number;
+  angle: number;
+  isWinner: boolean;
+};
+
+export default function DrawList({ total, win }: DrawListProps) {
+  const [cards, setCards] = useState<DrawItem[]>([]);
+  const [selected, setSelected] = useState<DrawItem>();
+  const [remainingWinner, setRemainingWinner] = useState<number>(win);
+
+  const width = 200;
+  let height: number = 400;
+
+  useEffect(() => {
+    const newCards = mixCards(total, remainingWinner);
+    setCards(newCards);
+    height = window.innerHeight - 178;
+  }, [total]);
+
+  return (
+    <div className="flex ">
+      {cards.map((card, index) => (
+        <button
+          className="absolute transition-all duration-300 ease-in-out"
+          key={card.num}
+          style={{
+            left: `calc(50% + ${-400 / 2 + card.x * width}px)`,
+            top: `${178 + card.y * height}px`,
+            rotate: `${card.angle}deg`,
+            zIndex: card !== selected ? index : 100,
+          }}
+          onClick={() => {
+            card.x = 0.5;
+            card.y = 0.5;
+            card.angle = 0;
+            setSelected(card);
+            if (card.isWinner) setRemainingWinner(prev => prev - 1);
+            setTimeout(() => {
+              setCards(prev => prev.filter(c => c.num !== card.num));
+            }, 3000);
+          }}
+        >
+          {card !== selected ? (
+            <Card color={card.color} borderColor={card.color}></Card>
+          ) : (
+            <Card color="white" borderColor={card.color}>
+              <div className="text-black w-full h-full flex items-center justify-center text-header font-semibold z-50">
+                {selected?.isWinner ? '당첨!' : `${(selected?.num ?? 0) + 1}`}
+              </div>
+            </Card>
+          )}
+        </button>
+      ))}
+
+      {cards.length > 0 ? (
+        <div className="absolute bottom-8 left-1/2 -translate-x-1/2">
+          <button
+            className="flex items-center gap-2"
+            onClick={() => setCards(mixCards(cards.length, remainingWinner, cards))}
+          >
+            <p className="font-semibold text-header"> 섞기 </p>
+            <FontAwesomeIcon icon={faRefresh} className="text-header"></FontAwesomeIcon>
+          </button>
+        </div>
+      ) : (
+        <div className="w-[400px] absolute bottom-8 left-1/2 -translate-x-1/2">
+          <Button
+            color="var(--color-menuYellow)"
+            content="다시 처음부터 뽑기"
+            textColor="black"
+            onClick={() => {
+              setRemainingWinner(win);
+              const newCards = mixCards(total, win);
+              setCards(newCards);
+            }}
+          ></Button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+function getRandomHexColor(): string {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  return `#${randomColor.padStart(6, '0')}`;
+}
+
+function mixCards(total: number, win: number, cards?: DrawItem[]): DrawItem[] {
+  let arr: number[] = Array.from(new Array(total), (_, i) => i);
+  arr.sort(() => Math.random() - 0.5);
+  console.log(arr);
+  return Array.from(cards || new Array(total), (_, i) => {
+    return {
+      x: Math.random(),
+      y: Math.random(),
+      color: getRandomHexColor(),
+      num: cards ? cards[i].num : arr[i],
+      angle: Math.random() * 360,
+      isWinner: cards ? cards[i].isWinner : arr[i] >= total - win,
+    };
+  });
+}
---
+++ src/app/draw/draw-result/page.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import Header from '@/components/Header';
+import DrawList from './draw-list';
+import { useSearchParams } from 'next/navigation';
+
+export default function DrawResult() {
+  const searchParams = useSearchParams();
+  const total = searchParams.get('total');
+  const win = searchParams.get('win');
+
+  return (
+    <div className="flex flex-col h-dvh">
+      <Header
+        title="제비뽑기"
+        goHome={false}
+        canSet={true}
+        onSet={() => {
+          console.log('hi');
+        }}
+      ></Header>
+      <div className=" flex flex-col gap-3 text-center font-semibold">
+        <p className="text-normal">
+          원하는 종이를 고르고 결과를 확인 후<br />
+          다음 사람에게 넘겨주세요.
+        </p>
+        <p className="text-description">※ 뽑힌 종이는 3초 후 사라집니다</p>
+      </div>
+      <DrawList total={Number(total)} win={Number(win)}></DrawList>
+    </div>
+  );
+}
