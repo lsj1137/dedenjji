44b90d10975162dabd38fb1c4eb1ce2583f6023c
LimSeJun <lsj1137@naver.com>
2025-05-14 23:20:02 +0900

    Fix: 가위바위보 인원 최솟값 2명 설정

--- src/app/rock-scissor-paper/page.tsx
+++ src/app/rock-scissor-paper/page.tsx
@@ -6,13 +6,13 @@ import Link from 'next/link';
 import { useState } from 'react';
 
 export default function RockScissorPaper() {
-  const [people, setPeople] = useState(1);
+  const [people, setPeople] = useState(2);
 
   return (
     <div>
       <Header title="가위바위보" goHome={false} canSet={false}></Header>
       <div className="my-20">
-        <Counter count={people} objectName="인원 수" minimum={1} onChange={setPeople}></Counter>
+        <Counter count={people} objectName="인원 수" minimum={2} onChange={setPeople}></Counter>
       </div>
       <Link
         href={{
