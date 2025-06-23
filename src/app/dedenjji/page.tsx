9dfc9f24b2f8ff9e55886119037cb1fc6a38e989
LimSeJun <lsj1137@naver.com>
2025-06-23 20:22:29 +0900

    fix : 데덴찌와 가위바위보 최소인원 제한

--- src/app/dedenjji/page.tsx
+++ src/app/dedenjji/page.tsx
@@ -2,11 +2,23 @@
 import Button from '@/components/Button';
 import Counter from '@/components/Counter';
 import Header from '@/components/Header';
+import showToast from '@/utils/toast';
 import Link from 'next/link';
 import { useState } from 'react';
 
 export default function Dedenjji() {
+  const peopleMinimum = 2;
   const [people, setPeople] = useState(2);
+
+  const handleClick = (e: React.MouseEvent) => {
+    if (people < peopleMinimum) {
+      e.preventDefault();
+      if (people < peopleMinimum) {
+        showToast(`인원 수는 ${peopleMinimum} 이상 이어야 해요!`);
+        setPeople(peopleMinimum);
+      }
+    }
+  };
 
   return (
     <div>
@@ -26,6 +38,7 @@ export default function Dedenjji() {
           pathname: '/dedenjji/result',
           query: { total: people },
         }}
+        onClick={handleClick}
       >
         <Button
           color="var(--color-menuGreen)"
--- src/app/rock-scissor-paper/page.tsx
+++ src/app/rock-scissor-paper/page.tsx
@@ -2,11 +2,23 @@
 import Button from '@/components/Button';
 import Counter from '@/components/Counter';
 import Header from '@/components/Header';
+import showToast from '@/utils/toast';
 import Link from 'next/link';
 import { useState } from 'react';
 
 export default function RockScissorPaper() {
+  const peopleMinimum = 2;
   const [people, setPeople] = useState(2);
+
+  const handleClick = (e: React.MouseEvent) => {
+    if (people < peopleMinimum) {
+      e.preventDefault();
+      if (people < peopleMinimum) {
+        showToast(`인원 수는 ${peopleMinimum} 이상 이어야 해요!`);
+        setPeople(peopleMinimum);
+      }
+    }
+  };
 
   return (
     <div>
@@ -19,6 +31,7 @@ export default function RockScissorPaper() {
           pathname: '/rock-scissor-paper/result',
           query: { total: people },
         }}
+        onClick={handleClick}
       >
         <Button color="var(--color-menuBlue)" content="대결 시작" onClick={() => {}}></Button>
       </Link>
